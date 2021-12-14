f79c9f5ebba4d8ca7ddc374de2543214cecdaffa
Love Lanai <love.lanai@medieinstitutet.se>
2021-12-15 00:57:49 +0100

    calculated how many days are in each month and created as many divs in the calendar as the result shows

--- main.js
+++ main.js
@@ -15,6 +15,18 @@ const month = date.getMonth();
 const dayNumber = date.getDate();
 const dayName = date.getDay();
 
+/** Hämtar ut hur många dagar som finns i varje månad */
+// 1, 0 = days
+// 1 = första dagen i månaden
+// 0 = första dagen i månaden -1. Alltså sista dagen i förra månaden
+const daysOfMonths = new Date(year, month + 1, 0).getDate();
+
+/** Hämtar ut första dagen i varje månad. Veckodag & nummer */
+// 1 = första dagen i månaden
+const firstDayOfMonths = new Date(year, month, 1);
+
+
+
 
 /** Översätter månaderna från siffror till Svenska */
 const monthNameSwedish = ["Januari", "Februari", "Mars", "April", "Maj", "Juni", "Juli", "Augusti", "September", "Oktober", "November", "December"]
@@ -24,17 +36,18 @@ const dayNameSwedish = ["Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lö
 
 /** Visar pågående månads namn i headern */
 function displayCurrentMonth() {
-    document.querySelector(".hej").innerHTML = monthNameSwedish[month];
+    document.querySelector(".displaymonth").innerHTML = monthNameSwedish[month];
 }
 
-/** Lägg till 31 divar med datum i kalendercontainern */
+
+/** Lägg till divar med datum i kalendercontainern */
 function addDaysToGrid() {
     // Hämtar ut div elementet från Html
     const calendarContent = document.getElementById('calendar-content');
-    // Lägger till 31st divar inuti calendarcontent-diven & ger dem innehåll & class
-    for(d=0; d<31;d++) {
+    // Lägger till divar ut efter antalet dagar i vald månad inuti calendar-content-diven & ger dem innehåll, id & class
+    for(let days=0; days <= daysOfMonths; days++) {
         var dayBoxes = document.createElement('div');
-        dayBoxes.innerHTML = (`<p>${dayNameSwedish[dayName]}</p>`)+(`<p>${dayNumber}</p>`);
+        dayBoxes.innerHTML = (`<p>${dayNameSwedish[dayName]}</p>`)+(`<p>${days+0}</p>`);
         dayBoxes.className = "days";
         dayBoxes.id = "dayboxes";
         calendarContent.appendChild(dayBoxes);
