b8d570b60d0e5632571e29b2e3eda90ff9c288f2
Love Lanai <love.lanai@medieinstitutet.se>
2021-12-14 17:43:16 +0100

    added header to display current month. Added day element and jsdoc

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ window.addEventListener('load', main);
 /** Kör alla funktioner i denna script onload */
 function main() {
     addDaysToGrid();
-    console.log(monthSwedish[month]);
+    displayCurrentMonth();
 }
 
 
@@ -16,33 +16,27 @@ const dayNumber = date.getDate();
 const dayName = date.getDay();
 
 
-/** Ge namn istället för nummer för månader & dagar */
-// Översätter månaderna från siffror till Svenska
-const monthSwedish = ["Januari", "Februari", "Mars", "April", "Maj", "Juni", "Juli", "Augusti", "September", "Oktober", "November", "December"]
-// Översätter veckodagarna från siffror till Svenska
+/** Översätter månaderna från siffror till Svenska */
+const monthNameSwedish = ["Januari", "Februari", "Mars", "April", "Maj", "Juni", "Juli", "Augusti", "September", "Oktober", "November", "December"]
+/** Översätter veckodagarna från siffror till Svenska */
 const dayNameSwedish = ["Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag", "Söndag"];
 
 
+/** Visar pågående månads namn i headern */
+function displayCurrentMonth() {
+    document.querySelector(".hej").innerHTML = monthNameSwedish[month];
+}
 
-
-
-
-// for (let j = 1; j <= nextDays; j++) {
-//     days += `<div class="next-date">${j}</div>`;
-//     daysMonthContainer.innerHTML = days;
-//   }
-// };
-
+/** Lägg till 31 divar med datum i kalendercontainern */
 function addDaysToGrid() {
+    // Hämtar ut div elementet från Html
     const calendarContent = document.getElementById('calendar-content');
+    // Lägger till 31st divar inuti calendarcontent-diven & ger dem innehåll & class
     for(d=0; d<31;d++) {
-        var board = document.createElement('div');
-        board.innerHTML = (dayNumber)
-        board.className = "blah";
-    
-        calendarContent.appendChild(board);
+        var dayBoxes = document.createElement('div');
+        dayBoxes.innerHTML = (`<p>${dayNumber}</p>`) + (`<p>${dayNameSwedish[dayName]}</p>`);
+        dayBoxes.className = "days";
+        calendarContent.appendChild(dayBoxes);
     }
-
-// calendarContent.innerHTML=(`<div>${dayNumber}</div>`);
 
 }
