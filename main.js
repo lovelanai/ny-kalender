5ceff6b18b0ad184f047544f46e0916b756b9f48
Love Lanai <love.lanai@medieinstitutet.se>
2021-12-15 15:29:00 +0100

    added js to elaborate calendar functionality

--- main.js
+++ main.js
@@ -11,27 +11,39 @@ function main() {
 /** Hämtar datum från server. Filtrerat År, månad, datum och veckodag. */
 const date = new Date();
 const year = date.getFullYear();
-const month = date.getMonth();
+let month = date.getMonth();
 const dayNumber = date.getDate();
 const dayName = date.getDay();
 
 /** Hämtar ut hur många dagar som finns i varje månad */
 // 1, 0 = days
-// 1 = första dagen i månaden
-// 0 = första dagen i månaden -1. Alltså sista dagen i förra månaden
+// 1 = Från början är januari=0, med +1 blir januari indexnummer 1.
+// 0 = Första dagen i nästa månad är 1, tar man då 0 får man sista dagen i nuvarande månad.
 const daysOfMonths = new Date(year, month + 1, 0).getDate();
 
 /** Hämtar ut första dagen i varje månad. Veckodag & nummer */
-// 1 = första dagen i månaden
-const firstDayOfMonths = new Date(year, month, 1);
+// 1 = Hämtar ut första datumet i månaden för att kunna placera ut på rätt veckodag.
+// T.ex. om 1 december är på en "onsdag".
+const firstDayOfMonths = new Date(year, month, 1, dayName);
+
+/** Hämtar ut sista dagen i förgående månad */
+const lastDaysOfPreviousMonth = new Date(year, month, 0).getDate();
+
+/**Får ut indexplatsen (Veckodagen) som den sista dagen i föregående månad har, söndag = 0*/
+const IndexOflastDaysOfPreviousMonth = new Date(year, month, 0).getDay();
 
 
+/** Hämtar ut första dagen i nästkommande månad */
+const firstDayOfNextMonth = new Date(year, month + 1, 1).getDate();
+
+/**Får ut indexplatsen (Veckodagen) som den första dagen i nästkommande månad har, söndag = 0*/
+const IndexOflFirstDaysOfNextMonth = new Date(year, month + 1, 1).getDay();
 
 
 /** Översätter månaderna från siffror till Svenska */
 const monthNameSwedish = ["Januari", "Februari", "Mars", "April", "Maj", "Juni", "Juli", "Augusti", "September", "Oktober", "November", "December"]
 /** Översätter veckodagarna från siffror till Svenska */
-const dayNameSwedish = ["Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag", "Söndag"];
+const dayNameSwedish = ["Söndag", "Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag"];
 
 
 /** Visar pågående månads namn i headern */
@@ -44,13 +56,35 @@ function displayCurrentMonth() {
 function addDaysToGrid() {
     // Hämtar ut div elementet från Html
     const calendarContent = document.getElementById('calendar-content');
+
+
     // Lägger till divar ut efter antalet dagar i vald månad inuti calendar-content-diven & ger dem innehåll, id & class
-    for(let days=0; days <= daysOfMonths; days++) {
+    for (let days = 1; days <= daysOfMonths; days++) {
         var dayBoxes = document.createElement('div');
-        dayBoxes.innerHTML = (`<p>${dayNameSwedish[dayName]}</p>`)+(`<p>${days+0}</p>`);
+        dayBoxes.innerHTML = (`<p>Veckodag</p>`) + (`<p>${days}</p>`);
         dayBoxes.className = "days";
         dayBoxes.id = "dayboxes";
         calendarContent.appendChild(dayBoxes);
     }
+}
+
+
+function displayPrevMonth() {
+    month = month - 1;
+
+    if (month < 0) {
+        month = 11;
+    }
+    document.querySelector(".displaymonth").innerHTML = monthNameSwedish[month];
+}
+
+function displayNextMonth() {
+    month = month + 1;
+
+    if (month > 11) {
+        month = 0;
+    }
+    document.querySelector(".displaymonth").innerHTML = monthNameSwedish[month];
 
 }
+
